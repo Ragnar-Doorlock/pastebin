50c8f953090556146079ab37fe6eacf93c9550f2
Mick <tr1al3xp@gmail.com>
2023-06-05 14:56:37 +0300

    created index

---
+++ app/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+//const RouterBuilder = require('');
+
+(async () => {
+
+    //const routes = new RouterBuilder({express});
+
+    //app.use(express.json());
+    //app.use('/', routes.createRoutes());
+
+    //app.listen(3000, () => console.log(`App listening on port ${3000}!`));
+
+})()
