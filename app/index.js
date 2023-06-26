f032f35e9e8aa5a72108c724f1206100cc9415a6
Mick <tr1al3xp@gmail.com>
2023-06-26 17:55:13 +0300

    removed comments from indexs

--- app/index.js
+++ app/index.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const app = express();
-//const RouterBuilder = require('');
-//require('dotenv').config();
 
 (async () => {
-    //const routes = new RouterBuilder({express});
-    //app.use(express.json());
-    //app.use('/', routes.createRoutes());
 
-    //app.listen(3000, () => console.log(`App listening on port ${3000}!`));
 
 })()
