e6abddebf1c7d15a8502be73a0dadf01fc45edf2
Mick <tr1al3xp@gmail.com>
2023-07-06 14:24:42 +0300

    singleton

--- db/dbProvider.js
+++ db/dbProvider.js
@@ -4,15 +4,10 @@ class DBProvider {
     }
 
     async execute(query) {
-        const client = await this.pool.connect();
-        const result = await client.query(query);
-        //await this.pool.end();
 
-        // now with this thing the app is running for some time after it executed query(-ies) (around 5 secs)
-        // but it seems better that pool.end since it doesn't let execute several queries at a time
-
-        await client.release();
+        const result = await this.pool.query(query);
         return result.rows;
+        
     }
 
 }
