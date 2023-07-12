8f5d940fd5c313d50eee2807a2cc7ae702b8e468
Mick <tr1al3xp@gmail.com>
2023-07-12 12:30:00 +0300

    renamed singleton class

---
+++ db/postgresPoolConnection.js
@@ -0,0 +1,27 @@
+const { Pool } = require("pg");
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, '../.env')});
+
+class PostgresPoolConnection {
+    constructor() {
+        this.pool = new Pool({
+            user: process.env.POSTGRES_DB_USERNAME,
+            host: process.env.POSTGRES_DB_HOST,
+            database: process.env.POSTGRES_DB_DATABASE,
+            password: process.env.POSTGRES_DB_PASSWORD,
+            port: process.env.POSTGRES_DB_PORT,
+        });
+    }
+
+    getInstance() {
+        if (this.pool) {
+            return this.pool;
+        }
+
+        this.pool = new Singleton();
+        return this.pool;
+    
+    }
+}
+
+module.exports = new PostgresPoolConnection();
