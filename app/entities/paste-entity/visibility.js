4d7faec4a3d23f067106043ec461cbec072fd100
Mick <tr1al3xp@gmail.com>
2023-07-14 15:13:35 +0300

    Manstrike Makarov said no russian

--- app/entities/paste-entity/visibility.js
+++ app/entities/paste-entity/visibility.js
@@ -2,8 +2,6 @@ const visibility = {
     PUBLIC: "public",
     PRIVATE: "private",
     SHARED: "shared"
-    // пускай пока будет shared, потом придумаю нормальное название
-    // это по смыслу как в гугл доках доступно тем, у кого ссылка
 }
 
 module.exports = visibility;
