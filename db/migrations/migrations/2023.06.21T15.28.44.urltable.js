0ba38ca2fe52075256878b640f9eb7d7f328eacf
Mick <tr1al3xp@gmail.com>
2023-08-17 15:20:14 +0300

    db table changes

--- db/migrations/migrations/2023.06.21T15.28.44.urltable.js
+++ db/migrations/migrations/2023.06.21T15.28.44.urltable.js
@@ -1,7 +1,7 @@
 /** @type {import('umzug').MigrationFn<any>} */
 exports.up = async ({context: sequelize}) => {await sequelize.query(`CREATE TABLE IF NOT EXISTS url (
     paste_id varchar(60) PRIMARY KEY,
-    hash VARCHAR ( 50 ),
+    url_id VARCHAR ( 60 ),
     FOREIGN KEY (paste_id) REFERENCES paste (id)
 )`)};
 
